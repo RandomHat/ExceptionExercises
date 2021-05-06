be8b51cfce79a7672301ec59c34f4e980f97f5f3
Roswall <simonroswall@gmail.com>
2021-05-06 09:38:39 +0200

    Handled FileNotFound Error and refactored test

--- test/NemIdAuthorizerTest.java
+++ test/NemIdAuthorizerTest.java
@@ -4,13 +4,13 @@ import java.io.FileNotFoundException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NemIdAuthorizerTest {
+    //Arrange
+    NemIdAuthorizer classUnderTest = new NemIdAuthorizer("Users/UserData.csv");
 
     public void testHasRepeatChars(){
-        //Arrange
-        NemIdAuthorizer testHasRepeatChars = new NemIdAuthorizer("Users/UserData.csv");
         //Act
-        boolean stringWith = testHasRepeatChars.hasRepeatChars("awegriiwægæhwæelhAAAA238402");
-        boolean stringWithout = testHasRepeatChars.hasRepeatChars("adælsælgjdæ308y5oæklfsdknglæ");
+        boolean stringWith = classUnderTest.hasRepeatChars("awegriiwægæhwæelhAAAA238402");
+        boolean stringWithout = classUnderTest.hasRepeatChars("adælsælgjdæ308y5oæklfsdknglæ");
         //Assert
         assertTrue(stringWith);
         assertFalse(stringWithout);
